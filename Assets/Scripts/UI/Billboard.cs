db6c83a63c59917d15335cf7c4dea51cddfe2e12
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-04-28 19:05:22 +0800

    4.28 final
    基本的ui复现，数据的读取，基本的逻辑跳转

--- Assets/Scripts/UI/Billboard.cs
+++ Assets/Scripts/UI/Billboard.cs
@@ -1,4 +1,5 @@
-﻿using Data;
+﻿using System;
+using Data;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -10,9 +11,28 @@ namespace UI
     {
         [SerializeField] private Transform billboardTransform;
         [SerializeField] private RectTransform content;
-
-        public void Content()
+        [SerializeField] private GameObject ItemPrefab;
+        
+        public Vector2 ItemRect { get { return new Vector2(968, 146); } }
+        private void Start()
         {
+            
+        }
+
+        public void SetContent(int length)
+        {
+            content.sizeDelta = new Vector2(ItemRect.x,length*ItemRect.y);
+        }
+
+        public void CreateItem()
+        {
+            //读取数据
+            DSItemData dsItemData = new DSItemData();
+            JsonDataManager.Instance.GetDataDSItem(ref dsItemData, "Json/data");
+            
+            int dataLength = dsItemData.list.Length; //获取json中的数据数量
+            
+            
             
         }
         public void CloseButtonOnClick()
---
+++ Assets/Scripts/UI/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class Item : MonoBehaviour
+    {
+        [SerializeField] private Image rank;
+        [SerializeField] private Text nickName;
+        [SerializeField] private Text trophy;
+        [SerializeField] private Image status;
+
+        [SerializeField] private List<Sprite> statusSprites;
+        [SerializeField] private List<Sprite> rankSprites;
+    }
+}
